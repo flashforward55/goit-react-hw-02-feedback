3d7ebbb9946ef998e8b04a1e6e5467811d17818f
Anton Styazhkin <rudana089@gmail.com>
2023-06-01 15:36:24 +0300

    Add Staristic.styled.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,11 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 
 class App extends Component {
-  state = {};
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
   render() {
     return (
       <AppContainer>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,14 @@
 //import PropTypes from 'prop-types';
+import { StatisticsContainer, StatisticItem } from './Statistics.styled';
 
-const Statistics = () => {};
+const Statistics = () => (
+  <StatisticsContainer>
+    <StatisticItem>Good: </StatisticItem>
+    <StatisticItem>Neutral: </StatisticItem>
+    <StatisticItem>Bad: </StatisticItem>
+    <StatisticItem>Total: </StatisticItem>
+    <StatisticItem>Positive feedback: %</StatisticItem>
+  </StatisticsContainer>
+);
 
 export default Statistics;
